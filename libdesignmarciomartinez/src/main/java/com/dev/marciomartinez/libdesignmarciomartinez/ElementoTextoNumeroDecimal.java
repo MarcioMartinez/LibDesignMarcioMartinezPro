32b478bacfcc5e5a96a8cca76e50d8e0170301e7
MarcioMartinez <marciomartinez500@gmail.com>
2019-06-11 14:38:10 -0600

    Arreglo error texto decimal

--- libdesignmarciomartinez/src/main/java/com/dev/marciomartinez/libdesignmarciomartinez/ElementoTextoNumeroDecimal.java
+++ libdesignmarciomartinez/src/main/java/com/dev/marciomartinez/libdesignmarciomartinez/ElementoTextoNumeroDecimal.java
@@ -21,6 +21,6 @@ public class ElementoTextoNumeroDecimal extends ElementoTextoNormal {
     @Override
     public void init(Context context, AttributeSet attributeSet) {
         super.init(context, attributeSet);
-        txtValor.setRawInputType(InputType.TYPE_NUMBER_FLAG_DECIMAL|InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS);
+        txtValor.setRawInputType(InputType.TYPE_NUMBER_FLAG_DECIMAL|InputType.TYPE_CLASS_NUMBER|InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS);
     }
 }
